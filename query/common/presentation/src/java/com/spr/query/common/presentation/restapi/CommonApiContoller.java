630f25a78b5ae2bddbcdb9e55a19807c35f981ce
ririsow <itt.devririso5328@gmail.com>
2025-02-17 21:29:42 +0900

    Contoller実装

--- query/common/presentation/src/java/com/spr/query/common/presentation/restapi/CommonApiContoller.java
+++ query/common/presentation/src/java/com/spr/query/common/presentation/restapi/CommonApiContoller.java
@@ -1,6 +1,29 @@
-package com.spr.query.common.presentation.restapi;
+package java.com.spr.query.common.presentation.restapi;
 
 
-public class CommonApiContoller implements CommonApi {
+import lombok.RequiredArgsConstructor;
+import main.java.com.spr.application.usecase.GetTasksUseCase;
+import main.java.com.spr.generated.model.GetCommonTasksResponse;
+import main.java.com.spr.generated.model.Task;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequiredArgsConstructor
+public class CommonApiContoller implements main.java.com.spr.generated.controller.CommonApi {
+    private final GetTasksUseCase getTasksUseCase;
+
+    @Override
+    public ResponseEntity<GetCommonTasksResponse> getTasks(Integer userId) {
+
+        final var tasksList = getTasksUseCase.execute(userId);
+
+        // TaskDtoのリストからGetCommonTasksResponse.Taskに変換
+        final var taskResponseList = tasksList.stream()
+                .map(task -> new Task(task.id(), task.name())) // task.id(), task.name() でデータを取り出し
+                .toList();
+
+        return ResponseEntity.ok(new GetCommonTasksResponse(taskResponseList));
+    }
 }
