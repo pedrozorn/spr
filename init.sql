25e9d779ba30fa2d5881656318c9880e621b3b82
ririsow <itt.devririso5328@gmail.com>
2025-02-28 20:42:57 +0900

    Docker関連のファイル追加

---
+++ init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE TaskDB;
+GO
+
+USE TaskDB;
+GO
+
+CREATE TABLE Tasks (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    TaskName NVARCHAR(255) NOT NULL,
+    IsDeleted BIT NOT NULL DEFAULT 0
+);
+GO
+
+INSERT INTO Tasks (TaskName, IsDeleted)
+VALUES
+    (N'Task 1', 0),
+    (N'Task 2', 1),
+    (N'Task 3', 0);
+GO
